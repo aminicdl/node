4bce1c86532e9f8cf305e5f42c68c945b353fa12
aminicdl <aminicdl2008@gmail.com>
2023-05-07 18:26:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,31 +17,19 @@ app.get('/webhook', function (req, res) {
 app.post('/webhook', function (req, res) {
   // we expect to receive JSON data from api.ai here.
   // the payload is stored on req.body
+  console.log(req.body)
   console.log(req.body.key)
 
-  // we have a simple authentication
-  // if (REQUIRE_AUTH) {
-  //   if (req.headers['auth-token'] !== AUTH_TOKEN) {
-  //     return res.status(401).send('Unauthorized')
-  //   }
-  // }
-
   // and some validation too
-  // if (!req.body || !req.body.result || !req.body.result.parameters) {
-  //   return res.status(400).send('Bad Request')
-  // }
+  if (!req.body ) {
+    return res.status(400).send('Bad Request')
+  }
 
-  // the value of Action from api.ai is stored in req.body.result.action
-  // console.log('* Received action -- %s', req.body.result.action)
-
-  // parameters are stored in req.body.result.parameters
-  // var userName = req.body.result.parameters['given-name']
-  // var webhookReply = 'Hello ' + userName + '! Welcome from the webhook.'
+  var webhookReply = 'Ok'
 
   // the most basic response
   res.status(200).json({
-    source: 'webhook'
-    // speech: webhookReply,
+    speech: webhookReply
     // displayText: webhookReply
   })
 })
