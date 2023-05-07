bebe875a8dd42811da7337b7ac1df11f88739ce2
aminicdl <aminicdl2008@gmail.com>
2023-05-07 18:15:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,29 +20,29 @@ app.post('/webhook', function (req, res) {
   console.log(req.body.key)
 
   // we have a simple authentication
-  if (REQUIRE_AUTH) {
-    if (req.headers['auth-token'] !== AUTH_TOKEN) {
-      return res.status(401).send('Unauthorized')
-    }
-  }
+  // if (REQUIRE_AUTH) {
+  //   if (req.headers['auth-token'] !== AUTH_TOKEN) {
+  //     return res.status(401).send('Unauthorized')
+  //   }
+  // }
 
   // and some validation too
-  if (!req.body || !req.body.result || !req.body.result.parameters) {
-    return res.status(400).send('Bad Request')
-  }
+  // if (!req.body || !req.body.result || !req.body.result.parameters) {
+  //   return res.status(400).send('Bad Request')
+  // }
 
   // the value of Action from api.ai is stored in req.body.result.action
-  console.log('* Received action -- %s', req.body.result.action)
+  // console.log('* Received action -- %s', req.body.result.action)
 
   // parameters are stored in req.body.result.parameters
-  var userName = req.body.result.parameters['given-name']
-  var webhookReply = 'Hello ' + userName + '! Welcome from the webhook.'
+  // var userName = req.body.result.parameters['given-name']
+  // var webhookReply = 'Hello ' + userName + '! Welcome from the webhook.'
 
   // the most basic response
   res.status(200).json({
-    source: 'webhook',
-    speech: webhookReply,
-    displayText: webhookReply
+    source: 'webhook'
+    // speech: webhookReply,
+    // displayText: webhookReply
   })
 })
 
